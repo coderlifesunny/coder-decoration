90ffdec08b074969a05a1ea8a509f88e52621df3
guangcai.xu <guangcai.xu@dmall.com>
2018-10-18 10:35:47 +0800

    初始化工程

---
+++ decoration-common/src/main/java/com/decoration/common/DecorationCommonApplication.java
@@ -0,0 +1,12 @@
+package com.decoration.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorationCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecorationCommonApplication.class, args);
+	}
+}
