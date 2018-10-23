74f9258ff0f713043c10c015fa0d4dabed622bd5
guangcai.xu <guangcai.xu@dmall.com>
2018-10-23 16:56:22 +0800

    修改一下拦截器日志打印

--- decoration-web/src/main/java/com/decoration/web/interceptor/GeneralInterceptor.java
+++ decoration-web/src/main/java/com/decoration/web/interceptor/GeneralInterceptor.java
@@ -1,5 +1,6 @@
 package com.decoration.web.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,23 +13,23 @@ import javax.servlet.http.HttpServletResponse;
  * @Author Guangcai.xu
  * @Date 2018/10/19 17:44
  */
+@Slf4j
 public class GeneralInterceptor implements HandlerInterceptor {
+
     //在请求处理之前进行调用（Controller方法调用之前
     @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-        System.out.printf("preHandle被调用\n");
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+        log.info("[" + request.getMethod() + "] " + request.getRemoteHost() + ":" + request.getRemotePort() + " " + request.getRequestURL());
         return true;
     }
 
     //请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）
     @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-        System.out.println("postHandle被调用\n");
     }
 
     //在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）
     @Override
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-        System.out.println("afterCompletion被调用\n");
     }
 }
